e4e0f704dd5ef86f253d65a8db24ca48ebf55252
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-05 21:46:06 +0100

    unnecessary lines deleted

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,14 @@
-import React from "react";
 import "./style.css";
 
 
-const Section = ({title, body, extraHeaderContent}) => (
+const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <header className="section__header">
             <h2 className="section__title">{title}</h2>
-           {extraHeaderContent}
+            {extraHeaderContent}
         </header>
         <div className="section__body">
-                {body}
+            {body}
         </div>
     </section>
 );
