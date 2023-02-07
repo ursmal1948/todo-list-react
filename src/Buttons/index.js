933f0b78c83a1398180b7bcd85412863c2b788b0
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-07 12:07:05 +0100

    deleted 'return' from funciton

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDoneTasks }) => {
-    return tasks.length > 0 && (
+const Buttons = ({ tasks, hideDoneTasks }) => (
+    tasks.length > 0 && (
         <div className="section__buttons">
-            <button className="buttons__button hideShowButton">
-                {hideDoneTasks === false ? "Ukryj" : "Pokaż"} ukończone
+            <button className="buttons__button">
+                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
-            <button className=" buttons__button completeAllButton"
+            <button className="buttons__button"
                 disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
             </button>
         </div>
     )
-};
+);
 
 export default Buttons;
