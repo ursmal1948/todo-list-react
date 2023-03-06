94ed3c8cd9e4faf6dae936bb978b7f46eb33fc71
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-06 22:15:23 +0100

    Hook useTasks created and moved to a seperate file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -7,57 +7,23 @@ import { StyledContainer } from './Container/styled';
 import { StyledHeader } from './Header/styled';
 import { ThemeProvider } from 'styled-components';
 import {theme} from "./theme"
+import { useTasks } from './useTasks';
 
-const getInitialTasks = () => {
-    const tasksFromLocalStorage = localStorage.getItem("tasks");
-
-    return tasksFromLocalStorage
-        ? JSON.parse(tasksFromLocalStorage)
-        : [];
-};
 
 function App() {
     const [hideDone, setHideDone] = useState(false)
-    const [tasks, setTasks] = useState(getInitialTasks); 
-
-    useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks))
-    }, [tasks])
 
     const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone)
     };
 
-    const removeTask = (id) => {
-        setTasks(tasks => tasks.filter(task => task.id !== id));
-    };
-
-    const toggleTaskDone = (id) => {
-        setTasks(tasks => tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, done: !task.done };
-            }
-            return task;
-        }))
-    }
-
-    const setAllDone = () => {
-        setTasks(tasks => tasks.map(task => (
-            { ...task, done: true, }
-        )));
-    }
-
-    const addNewTask = (newTaskContent) => {
-
-        setTasks(tasks => [
-            ...tasks,
-            {
-                content: newTaskContent,
-                done: false,
-                id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-            },
-        ]);
-    };
+    const {
+        tasks, 
+        removeTask, 
+        toggleTaskDone,
+        setAllDone,
+        addNewTask
+     } = useTasks();
 
     return (
         <ThemeProvider theme={theme}>
