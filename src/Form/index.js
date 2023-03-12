4b6272c775bf9d7bc45e4918cb85be13515ec559
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-12 13:29:52 +0100

    refactoring of onFormSubmit function

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,38 +1,37 @@
 import { useState, useRef } from "react";
-import {StyledForm, Input, Button } from "./styled";
+import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ title, addNewTask, className }) => {
+  const inputRef = useRef(null);
+  const setFocus = () => {
+    inputRef.current.focus();
+  };
 
-    const inputRef = useRef(null)
-    const setFocus = () => {
-        inputRef.current.focus();
-    };
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const [newTaskContent, setNewTaskContent] = useState("");
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const trimmedNewTaskContent = newTaskContent.trim();
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        const trimmedNewTaskContent = newTaskContent.trim();
-        trimmedNewTaskContent !== "" && (
-            addNewTask(trimmedNewTaskContent)
-        );
-        setNewTaskContent("");
-        setFocus();
-    };
+    if(!trimmedNewTaskContent) {
+        return ;
+    }
+    addNewTask(trimmedNewTaskContent);
+    setNewTaskContent("");
+    setFocus();
+  };
 
-    return (
-        <StyledForm className={className} onSubmit={onFormSubmit}>
-            <Input
-                ref={inputRef}
-                value={newTaskContent}
-                placeholder="Co jest do zrobienia?"
-                onChange={(event) => setNewTaskContent(event.target.value)}
-            />
-            <Button>
-                {title}
-            </Button>
-        </StyledForm>
-    );
+  return (
+    <StyledForm className={className} onSubmit={onFormSubmit}>
+      <Input
+        ref={inputRef}
+        value={newTaskContent}
+        placeholder="Co jest do zrobienia?"
+        onChange={(event) => setNewTaskContent(event.target.value)}
+      />
+      <Button>{title}</Button>
+    </StyledForm>
+  );
 };
 
 export default Form;
